df5e59f53cd2967853d7be657f9be840559c02d1
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-11 11:20:50 +0100

    Delete Services directory

--- Services/TmdbBoxset.cs
+++
@@ -1,9 +0,0 @@
-namespace TmdbBoxsetMetadataForCollections.Services;
-
-public class TmdbBoxset
-{
-    public string Name { get; set; }
-    public string Overview { get; set; }
-    public string Poster_Path { get; set; }
-    public string Backdrop_Path { get; set; }
-}
