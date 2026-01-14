b30d4d7600754d3ed7f000d2eef67d3e435e2185
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-14 20:20:03 +0100

    Update Plugin.cs

--- Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Plugin.cs
+++ Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Plugin.cs
@@ -3,19 +3,19 @@ using System;
 
 namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections
 {
-    public class Plugin : BasePlugin
+    public sealed class Plugin : BasePlugin
     {
-        public Plugin(IApplicationPaths applicationPaths)
-            : base(applicationPaths)
+        public static Plugin? Instance { get; private set; }
+
+        public Plugin()
         {
             Instance = this;
         }
 
-        public static Plugin? Instance { get; private set; }
-
         public override string Name => "TMDb Boxset Metadata for Collections";
 
-        public override Guid Id => new Guid("b11c1cde-4c6e-4c55-b4a5-5a4b95f7c801");
+        public override Guid Id =>
+            new Guid("b11c1cde-4c6e-4c55-b4a5-5a4b95f7c801");
 
         public override string Description =>
             "Automatically assigns TMDb boxset metadata to existing Jellyfin collections based on their movies.";
