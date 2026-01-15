977ac2476513b8c5227ba6ca23d8fd8887a92cf0
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-15 17:00:26 +0100

    Update ServiceRegistrator.cs

--- Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/ServiceRegistrator.cs
+++ Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/ServiceRegistrator.cs
@@ -1,6 +1,6 @@
+using MediaBrowser.Controller;
 using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
-using Jellyfin.Server;
 
 namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections
 {
