22c79a47fa8a9a98d72640ac67336d31bde6184a
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-16 16:31:21 +0100

    Create ProviderKeys.cs

---
+++ Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/ProviderKeys.cs
@@ -0,0 +1,8 @@
+namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections
+{
+    public static class ProviderKeys
+    {
+        // Das ist das Feld, das Jellyfin bei Filmen/BoxSets benutzt.
+        public const string TmdbCollection = "TmdbCollection";
+    }
+}
