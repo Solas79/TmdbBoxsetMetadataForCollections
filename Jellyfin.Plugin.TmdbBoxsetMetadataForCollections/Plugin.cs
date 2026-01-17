3894ace3b8ddca5d6c7a67905d8a32d9d9e98618
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-17 16:05:25 +0100

    Update Plugin.cs

--- Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Plugin.cs
+++ Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Plugin.cs
@@ -3,36 +3,31 @@
 // Licensed under the GNU General Public License v2.0.
 // </copyright>
 
-using System;
-using MediaBrowser.Common.Plugins;
-
-namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections;
-
-/// <summary>
-/// Plugin entry point.
-/// </summary>
-public sealed class Plugin : BasePlugin
+namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections
 {
-    /// <summary>
-    /// Gets the plugin instance.
-    /// </summary>
-    public static Plugin Instance { get; private set; } = null!;
+    using System;
+    using MediaBrowser.Common.Plugins;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="Plugin"/> class.
+    /// Plugin entry point.
     /// </summary>
-    public Plugin()
+    public sealed class Plugin : BasePlugin
     {
-        Instance = this;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Plugin"/> class.
+        /// </summary>
+        public Plugin()
+        {
+        }
+
+        /// <inheritdoc />
+        public override string Name => "TMDb Boxset Metadata for Collections";
+
+        /// <inheritdoc />
+        public override Guid Id => new Guid("b11c1cde-4c6e-4c55-b4a5-5a4b95f7c801");
+
+        /// <inheritdoc />
+        public override string Description =>
+            "Assigns TMDb boxset metadata to existing Jellyfin collections based on their movies. Manual task only.";
     }
-
-    /// <inheritdoc />
-    public override string Name => "TMDb Boxset Metadata for Collections";
-
-    /// <inheritdoc />
-    public override Guid Id => new("b11c1cde-4c6e-4c55-b4a5-5a4b95f7c801");
-
-    /// <inheritdoc />
-    public override string Description =>
-        "Assigns TMDb boxset/collection ids to existing Jellyfin collections based on their movies. Manual scan only.";
 }
