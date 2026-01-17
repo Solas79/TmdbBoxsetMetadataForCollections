dfa8e4b11b7363f6f4fdfbd981bbb2cfe0e77c24
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-17 11:52:30 +0100

    Delete Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Configuration directory

--- Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Configuration/PluginConfiguration.cs
+++
@@ -1,9 +0,0 @@
-using MediaBrowser.Model.Plugins;
-
-namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections.Configuration
-{
-    public sealed class PluginConfiguration : BasePluginConfiguration
-    {
-        public bool LogDebug { get; set; } = false;
-    }
-}
