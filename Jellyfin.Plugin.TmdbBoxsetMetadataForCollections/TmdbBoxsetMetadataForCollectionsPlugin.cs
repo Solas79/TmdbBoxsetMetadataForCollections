b76581315b3723b56a6e929d4f3356cddf75c90d
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-17 17:01:15 +0100

    Update TmdbBoxsetMetadataForCollectionsPlugin.cs

--- Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/TmdbBoxsetMetadataForCollectionsPlugin.cs
+++ Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/TmdbBoxsetMetadataForCollectionsPlugin.cs
@@ -1,1 +1,33 @@
-1
+// <copyright file="Plugin.cs" company="Jellyfin">
+// Copyright (c) Jellyfin.
+// Licensed under the GNU General Public License v2.0.
+// </copyright>
+
+namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections
+{
+    using System;
+    using MediaBrowser.Common.Plugins;
+
+    /// <summary>
+    /// Plugin entry point.
+    /// </summary>
+    public sealed class TmdbBoxsetMetadataForCollectionsPlugin : BasePlugin
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TmdbBoxsetMetadataForCollectionsPlugin"/> class.
+        /// </summary>
+        public TmdbBoxsetMetadataForCollectionsPlugin()
+        {
+        }
+
+        /// <inheritdoc />
+        public override string Name => "TMDb Boxset Metadata for Collections";
+
+        /// <inheritdoc />
+        public override Guid Id => new Guid("b11c1cde-4c6e-4c55-b4a5-5a4b95f7c801");
+
+        /// <inheritdoc />
+        public override string Description =>
+            "Copies ProviderIds['TmdbCollection'] from movies into their collections and refreshes metadata (manual task).";
+    }
+}
