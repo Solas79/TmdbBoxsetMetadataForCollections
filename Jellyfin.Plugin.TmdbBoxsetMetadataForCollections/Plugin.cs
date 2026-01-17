de653e0f683a112b5bd038493ddbf8a13b89d9aa
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-17 10:34:00 +0100

    Update Plugin.cs

--- Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Plugin.cs
+++ Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Plugin.cs
@@ -1,22 +1,15 @@
 using System;
-using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
-using MediaBrowser.Model.Serialization;
 
 namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections
 {
     public sealed class Plugin : BasePlugin
     {
-        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
-            : base(applicationPaths, xmlSerializer)
-        {
-        }
-
         public override string Name => "TMDb Boxset Metadata for Collections";
 
         public override Guid Id => Guid.Parse("b11c1cde-4c6e-4c55-b4a5-5a4b95f7c801");
 
         public override string Description =>
-            "Copies TMDb Collection IDs from movies into BoxSets (Collections) so Jellyfin can download boxset metadata/images.";
+            "Copies TMDbCollection IDs from movies into BoxSets (Collections) and triggers metadata/image refresh.";
     }
 }
