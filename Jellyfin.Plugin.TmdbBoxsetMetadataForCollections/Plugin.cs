cc3873189374d8c60adee45489442c8cc0168cdc
Salas79 <144158482+Solas79@users.noreply.github.com>
2026-01-18 13:48:58 +0100

    Update Plugin.cs

--- Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Plugin.cs
+++ Jellyfin.Plugin.TmdbBoxsetMetadataForCollections/Plugin.cs
@@ -5,28 +5,17 @@ using MediaBrowser.Model.Serialization;
 
 namespace Jellyfin.Plugin.TmdbBoxsetMetadataForCollections;
 
-/// <summary>
-/// Plugin entry point.
-/// </summary>
 public sealed class Plugin : BasePlugin
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Plugin"/> class.
-    /// </summary>
-    /// <param name="applicationPaths">Application paths.</param>
-    /// <param name="xmlSerializer">XML serializer.</param>
     public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
         : base(applicationPaths, xmlSerializer)
     {
     }
 
-    /// <inheritdoc />
     public override string Name => "TMDb Boxset Metadata for Collections";
 
-    /// <inheritdoc />
     public override Guid Id => new Guid("b11c1cde-4c6e-4c55-b4a5-5a4b95f7c801");
 
-    /// <inheritdoc />
     public override string Description =>
-        "Sets ProviderIds['Tmdb'] on BoxSets using first contained movie's ProviderIds['TmdbCollection'] (manual task).";
+        "Manual task: copies Movie.ProviderIds['TmdbCollection'] into BoxSet.ProviderIds['Tmdb'] so metadata/artwork can be fetched.";
 }
